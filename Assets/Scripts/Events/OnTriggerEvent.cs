afe560199d575501cd8c0e37f626dc2db46e46c0
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-05-08 16:17:29 +1000

    Dash Done

---
+++ Assets/Scripts/Events/OnTriggerEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEvent : MonoBehaviour
+{
+    public string hitTag;
+    public UnityEvent onEnter,onStay,onExit;
+
+    private void Reset()
+    {
+        Collider col = GetComponent<Collider>();
+        if (col)
+        {
+            col.isTrigger = true;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == hitTag || hitTag == "") // || == "OR"
+        {
+            onEnter.Invoke();
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == hitTag || hitTag == "")
+        {
+            onStay.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == hitTag || hitTag == "")
+        {
+            onExit.Invoke();
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,108 +4,137 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float runSpeed = 10f;
+    // Variables
+    public float runSpeed = 8f;
     public float walkSpeed = 6f;
-    public float gravity = -30f;
-    public float jumpHeight = 10f;
+    public float dashSpeed = 20f;
+    public float dashTime = 2f;
+    public float gravity = -10f;
+    public float jumpHeight = 15f;
     public float groundRayDistance = 1.1f;
     private CharacterController controller; // Reference to character controller
     private Vector3 motion; // Is the movement offset per frame
-    private bool isJumping = false;
+    private bool isJumping;
+    private float currentJumpHeight;
+    private float currentSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    // Functions
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
+        currentSpeed = walkSpeed;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // W A S D / Right Left Up Down Arrow Input
         float inputH = Input.GetAxis("Horizontal");
         float inputV = Input.GetAxis("Vertical");
         // Left Shift Input
-        bool inputRun = Input.GetKey(KeyCode.LeftShift);
+        bool inputRun = Input.GetKeyDown(KeyCode.LeftShift);
+        bool inputWalk = Input.GetKeyUp(KeyCode.LeftShift);
+        bool inputDash = Input.GetKeyDown(KeyCode.E);
         // Space Bar Input
-        bool inputJump = Input.GetButton("Jump");
-
+        bool inputJump = Input.GetButtonDown("Jump");
         // Put Horizontal & Vertical input into vector
         Vector3 inputDir = new Vector3(inputH, 0f, inputV);
         // Rotate direction to Player's Direction
         inputDir = transform.TransformDirection(inputDir);
         // If input exceeds length of 1
-        if(inputDir.magnitude > 1)
+        if (inputDir.magnitude > 1f)
         {
             // Normalize it to 1f!
             inputDir.Normalize();
-        }        
+        }
 
+        if (inputDash)
+        {
+            Dash();
+        }
+
+        // If running
         if (inputRun)
         {
-            Run(inputDir.x, inputDir.z);
-        }
-        else
-        {
-            Walk(inputDir.x, inputDir.z);
+            currentSpeed = runSpeed;
         }
 
-        // If player is on the ground AND pressed "Jump"
-        if (IsGrounded() && inputJump)
+        if (inputWalk)
         {
-            Jump();
+            currentSpeed = walkSpeed;
         }
 
-        // If is NOT Grounded AND isJumping
-        if(!IsGrounded() && isJumping)
+        Move(inputDir.x, inputDir.z, currentSpeed);
+
+        // If is Grounded
+        if (controller.isGrounded)
         {
-            // Set isJumping to false
-            isJumping = false;
+            // .. And jump?
+            if (inputJump)
+            {
+                Jump(jumpHeight);
+            }
+
+            // Cancel the y velocity
+            motion.y = 0f;
+
+            // Is jumping bool set to true
+            if (isJumping)
+            {
+                // Set jump height
+                motion.y = currentJumpHeight;
+                // Reset back to false
+                isJumping = false;
+            }
         }
+
 
         motion.y += gravity * Time.deltaTime;
-
         controller.Move(motion * Time.deltaTime);
     }
-
-    bool IsGrounded()
-    {
-        // Raycast below the Player
-        Ray groundRay = new Ray(transform.position, -transform.up);
-        RaycastHit hit;
-        // If hitting something
-        if (Physics.Raycast(groundRay, out hit, groundRayDistance))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    void Move(float inputH, float inputV, float speed)
+    private void Move(float inputH, float inputV, float speed)
     {
         Vector3 direction = new Vector3(inputH, 0f, inputV);
-
-        // Convert local direction to world space direction (relative to Player's transform) 
-        // direction = transform.TransformDirection(direction);
-
         motion.x = direction.x * speed;
         motion.z = direction.z * speed;
     }
+    IEnumerator SpeedBoost(float startDash, float endDash, float delay)
+    {
+        currentSpeed = startDash;
 
+        yield return new WaitForSeconds(delay);
+
+        currentSpeed = endDash;
+    }
     public void Walk(float inputH, float inputV)
     {
         Move(inputH, inputV, walkSpeed);
     }
-
     public void Run(float inputH, float inputV)
     {
         Move(inputH, inputV, runSpeed);
     }
-
-    public void Jump()
+    public void Jump(float height)
     {
-        motion.y = jumpHeight;
-        isJumping = true;
+        isJumping = true; // We are jumping!
+        currentJumpHeight = height;
     }
-
+    public void Dash()
+    {
+        StartCoroutine(SpeedBoost(dashSpeed, walkSpeed, dashTime));
+    }
 }
+
+
+
+
+//bool IsGrounded()
+//{
+// Raycast below the Player
+//Ray groundRay = new Ray(transform.position, -transform.up);
+//RaycastHit hit;
+// If hitting something
+//if (Physics.Raycast(groundRay, out hit, groundRayDistance))
+//{
+//return true;
+//}
+//return false;
+//}
