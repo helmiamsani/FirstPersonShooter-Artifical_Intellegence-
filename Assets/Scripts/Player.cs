83c2d179e132c4e676bbb2b19aef9715b65a52ba
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-05-08 14:30:21 +1000

    Updated Stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float horizontalRunSpeed, verticalRunSpeed;
+    public float runSpeed = 10f;
     public float walkSpeed = 6f;
-    public float gravity = 10f;
-    public float jumpHeight = 15f;
+    public float gravity = -30f;
+    public float jumpHeight = 10f;
     public float groundRayDistance = 1.1f;
-
     private CharacterController controller; // Reference to character controller
     private Vector3 motion; // Is the movement offset per frame
-
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,27 +22,48 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // W A S D / Right Left Up Down Arrow Input
         float inputH = Input.GetAxis("Horizontal");
-        float inputV = Input.GetAxis("Vertical"); 
-        Move(inputH, inputV);      
+        float inputV = Input.GetAxis("Vertical");
+        // Left Shift Input
+        bool inputRun = Input.GetKey(KeyCode.LeftShift);
+        // Space Bar Input
+        bool inputJump = Input.GetButton("Jump");
 
-        if (IsGrounded())
+        // Put Horizontal & Vertical input into vector
+        Vector3 inputDir = new Vector3(inputH, 0f, inputV);
+        // Rotate direction to Player's Direction
+        inputDir = transform.TransformDirection(inputDir);
+        // If input exceeds length of 1
+        if(inputDir.magnitude > 1)
         {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                Move(inputH * 2f, inputV * 2f);
+            // Normalize it to 1f!
+            inputDir.Normalize();
+        }        
 
-                horizontalRunSpeed = motion.x;
-                verticalRunSpeed = motion.z;
-            }
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                motion.y = jumpHeight;
-            }
+        if (inputRun)
+        {
+            Run(inputDir.x, inputDir.z);
+        }
+        else
+        {
+            Walk(inputDir.x, inputDir.z);
         }
 
-        motion.y -= gravity * Time.deltaTime;
+        // If player is on the ground AND pressed "Jump"
+        if (IsGrounded() && inputJump)
+        {
+            Jump();
+        }
+
+        // If is NOT Grounded AND isJumping
+        if(!IsGrounded() && isJumping)
+        {
+            // Set isJumping to false
+            isJumping = false;
+        }
+
+        motion.y += gravity * Time.deltaTime;
 
         controller.Move(motion * Time.deltaTime);
     }
@@ -54,21 +74,38 @@ public class Player : MonoBehaviour
         Ray groundRay = new Ray(transform.position, -transform.up);
         RaycastHit hit;
         // If hitting something
-        if(Physics.Raycast(groundRay, out hit, groundRayDistance))
+        if (Physics.Raycast(groundRay, out hit, groundRayDistance))
         {
             return true;
         }
         return false;
     }
 
-    void Move(float inputH, float inputV)
+    void Move(float inputH, float inputV, float speed)
     {
         Vector3 direction = new Vector3(inputH, 0f, inputV);
-        
-        // Convert local direction to world space direction (relative to Player's transform) 
-        direction = transform.TransformDirection(direction);
 
-        motion.x = direction.x * walkSpeed;
-        motion.z = direction.z * walkSpeed;
+        // Convert local direction to world space direction (relative to Player's transform) 
+        // direction = transform.TransformDirection(direction);
+
+        motion.x = direction.x * speed;
+        motion.z = direction.z * speed;
     }
+
+    public void Walk(float inputH, float inputV)
+    {
+        Move(inputH, inputV, walkSpeed);
+    }
+
+    public void Run(float inputH, float inputV)
+    {
+        Move(inputH, inputV, runSpeed);
+    }
+
+    public void Jump()
+    {
+        motion.y = jumpHeight;
+        isJumping = true;
+    }
+
 }
