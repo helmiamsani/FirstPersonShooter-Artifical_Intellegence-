76aec6986a581f5ec392a41956838f68ba7c7b52
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-06-03 13:04:21 +1000

    xx

---
+++ Assets/Scripts/AI/AI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public float maxVelocity = 15f, maxDistance = 10f;
+    public Vector3 velocity;
+    public SteeringBehaviour[] behaviours;
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AI/SteeringBehaviour/Seek.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : SteeringBehaviour
+{
+    public override Vector3 GetForce(AI owner)
+    {
+        // SET force to zero
+        Vector3 force = Vector3.zero;
+        // Implement SEEK here
+        // RETURN force
+        return force;
+    }
+}
